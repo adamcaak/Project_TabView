66a06208dc508e1aa151057886980488541d17db
adamcaak <ladamczak1997@gmail.com>
2025-01-21 22:14:35 +0100

    Initial Commit

---
+++ Project_TabView/Project_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TabViewApp.swift
+//  Project_TabView
+//
+//  Created by Łukasz Adamczak on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
